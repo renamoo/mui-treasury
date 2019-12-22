5a62259bcf143a439c56a6168f3dd1c2c5a17792
renamoo <renamoo808@gmail.com>
2019-12-22 11:45:57 +0900

    refs #213 add tag card

--- website/src/docs/components/card/tag/TagCard.js
+++ website/src/docs/components/card/tag/TagCard.js
@@ -3,46 +3,15 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import RestaurantMenuRounded from '@material-ui/icons/RestaurantMenuRounded';
 import TripOrigin from '@material-ui/icons/TripOrigin';
-import { makeStyles } from '@material-ui/styles';
-import cx from 'clsx';
+import { useTagCardStyles } from '@mui-treasury/styles/cardContent/tag';
 import React from 'react';
-const mainColor = '#FFFFFF';
-
-const useStyles = makeStyles(({ palette, breakpoints }) => ({
-  card: {
-    width: '180px',
-    height: '230px',
-    margin: '20px 0 0 0',
-    display: 'flex',
-    background: mainColor,
-    position: 'relative',
-    overflow: 'visible',
-    '&::before': {
-      content: "' '",
-      width: '100%',
-      'border-bottom': `30px solid ${mainColor}`,
-      'border-left': ' 20px solid transparent',
-      'border-right': '20px solid transparent',
-      position: 'absolute',
-      top: '-30px',
-    },
-  },
-  hole: {
-    position: 'absolute',
-    top: '-20px',
-    margin: 'auto',
-    width: '100%',
-    left: 0,
-  },
-}));
 
 const TagCard = () => {
-  const styles = useStyles();
+  const tagStyles = useTagCardStyles();
   return (
-    <Card className={styles.card} elevation={0}>
-      <div className={cx(styles.left, styles.moveLeft)}></div>
+    <Card className={tagStyles.card} elevation={0}>
       <CardContent>
-        <TripOrigin className={styles.hole} />
+        <TripOrigin className={tagStyles.hole} />
         <RestaurantMenuRounded
           fontSize="inherit"
           style={{ fontSize: '83px', margin: 'auto', width: '100%' }}
@@ -65,13 +34,15 @@ const TagCard = () => {
   );
 };
 
-// TagCard.propTypes = {};
-// TagCard.defaultProps = {};
-
 TagCard.metadata = {
-  title: 'Tag', // name that appear in web
+  title: 'Tag Card',
   path: 'card/tag', // reference to markdown file
-  files: [],
+  files: [
+    {
+      pkg: 'mui-styles',
+      path: 'cardContent/tag/tagCard.styles.js',
+    },
+  ],
   relates: [],
   frameProps: {
     bgcolor: '#e9e9e9',
